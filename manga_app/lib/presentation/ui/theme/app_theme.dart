81f7e27e1ad88ff6bb7717a999a1d2a3aa9fa7f3
Angelo Lentini <angelolentini1409@gmail.com>
2025-09-30 16:13:33 +0200

    appBar theme

--- manga_app/lib/presentation/ui/theme/app_theme.dart
+++ manga_app/lib/presentation/ui/theme/app_theme.dart
@@ -30,6 +30,15 @@ ThemeData appTheme({bool dark = false}) {
     scaffoldBackgroundColor: theme.colors.backgroundColor,
     extensions: [theme.colors, theme.style],
 
+    // TODO: definire altri temi globali come bottoni, input, ecc.
+    // TODO: appBar da aggiornare secondo il figma
+    appBarTheme: AppBarTheme(
+      backgroundColor: theme.colors.primaryColor, // colore di sfondo
+      foregroundColor: theme.colors.textPrimary, // colore del titolo e icone
+      elevation: 0, // opzionale: app bar flat
+      centerTitle: true, // opzionale: centra il titolo
+    ),
+
     floatingActionButtonTheme: FloatingActionButtonThemeData(
       backgroundColor: theme.colors.primaryColor,
       foregroundColor: theme.colors.textPrimary,
