ece998f57b439efeb8668a27a6a6bdef44e8f7f5
Angelo Lentini <angelolentini1409@gmail.com>
2025-10-03 12:43:18 +0200

    Refactor appBar

--- manga_app/lib/presentation/ui/screens/home_page.dart
+++ manga_app/lib/presentation/ui/screens/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:manga_app/presentation/ui/theme/app_colors.dart';
 import 'package:manga_app/presentation/ui/theme/app_text_style.dart';
 import 'package:manga_app/presentation/ui/widgets/swiper_with_text.dart';
 import 'package:manga_app/presentation/ui/widgets/highlighted_section.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -15,6 +15,27 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: colors.backgroundColor,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: SvgPicture.asset(
+            'assets/icons/app_logo.svg',
+            width: 24,
+            height: 24,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search, color: colors.textSecondary, size: 32),
+            onPressed: () {
+              // Implementare la logica di ricerca
+            },
+          ),
+        ],
+      ),
       body: Column(
         children: [
           // Stack con lo swiper e le icone
@@ -27,44 +48,6 @@ class HomePage extends StatelessWidget {
                   'assets/images/episode3.jpg',
                 ],
                 titles: ['Titolo Opera 1', 'Titolo Opera 2', 'Titolo Opera 3'],
-              ),
-              // Logo dell'app in alto a sinistra
-              Positioned(
-                top: 16,
-                left: 24,
-                child: SafeArea(
-                  child: Container(
-                    width: 32,
-                    height: 32,
-                    child: SvgPicture.asset(
-                      'assets/icons/app_logo.svg',
-                      width: 32,
-                      height: 32,
-                    ),
-                  ),
-                ),
-              ),
-              // Icona di ricerca
-              Positioned(
-                top: 16,
-                right: 24,
-                child: SafeArea(
-                  child: Container(
-                    width: 32,
-                    height: 32,
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.search,
-                        color: colors.textSecondary,
-                        size: 28,
-                      ),
-                      onPressed: () {
-                        // TODO: Implementare la funzionalità di ricerca
-                        print('Search pressed');
-                      },
-                    ),
-                  ),
-                ),
               ),
             ],
           ),
