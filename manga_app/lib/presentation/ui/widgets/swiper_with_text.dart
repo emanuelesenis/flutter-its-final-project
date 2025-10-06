1b016851ee0c98e3d71ec5c58c559d08f9d442cf
Emanuele Alessandro Senis <emanuele.senis@edu-its.it>
2025-10-06 17:47:44 +0200

    minor fixes

--- manga_app/lib/presentation/ui/widgets/swiper_with_text.dart
+++ manga_app/lib/presentation/ui/widgets/swiper_with_text.dart
@@ -10,10 +10,10 @@ class SwiperWithText extends StatelessWidget {
   final List<String> titles;
 
   const SwiperWithText({
-    Key? key,
+    super.key,
     required this.imagePaths,
     required this.titles,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -26,12 +26,12 @@ class SwiperWithText extends StatelessWidget {
       child: Swiper(
         itemCount: imagePaths.length,
         itemBuilder: (BuildContext context, int index) {
-          return Container(
+          return SizedBox(
             width: double.infinity,
             child: Stack(
               children: [
                 // Immagine di sfondo
-                Container(
+                SizedBox(
                   width: double.infinity,
                   height: double.infinity,
                   child: Image.asset(
