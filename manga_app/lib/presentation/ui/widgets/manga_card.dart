77a76020efec84bd37dbd5fca79113a28ab0f393
Angelo Lentini <angelolentini1409@gmail.com>
2025-10-27 14:43:41 +0100

    fix worning

--- manga_app/lib/presentation/ui/widgets/manga_card.dart
+++ manga_app/lib/presentation/ui/widgets/manga_card.dart
@@ -15,50 +15,48 @@ class MangaCard extends StatelessWidget {
     final cardHeight =
         context.screenHeight * 0.15; // Ridotta altezza per evitare overflow
 
-    return Flexible(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          GestureDetector(
-            onTap: () {
-              context.push('/details/${manga.id}');
-            },
-            child: Container(
-              width: cardWidth,
-              height: cardHeight,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                image: DecorationImage(
-                  image: NetworkImage(
-                    manga.cover,
-                  ), // Use NetworkImage for network URLs
-                  fit: BoxFit.cover,
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withAlpha(25),
-                    blurRadius: 6,
-                    offset: const Offset(0, 4),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(height: 8),
-          SizedBox(
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        GestureDetector(
+          onTap: () {
+            context.push('/details/${manga.id}');
+          },
+          child: Container(
             width: cardWidth,
-            child: Text(
-              manga.title ?? 'No Title',
-              textAlign: TextAlign.center,
-              style: context.textStyles.h4.copyWith(
-                fontWeight: FontWeight.w600,
-                fontSize: 12, // Ridotto il font per adattare il titolo
+            height: cardHeight,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              image: DecorationImage(
+                image: NetworkImage(
+                  manga.cover,
+                ), // Use NetworkImage for network URLs
+                fit: BoxFit.cover,
               ),
-              overflow: TextOverflow.ellipsis,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withAlpha(25),
+                  blurRadius: 6,
+                  offset: const Offset(0, 4),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+        const SizedBox(height: 8),
+        SizedBox(
+          width: cardWidth,
+          child: Text(
+            manga.title ?? 'No Title',
+            textAlign: TextAlign.center,
+            style: context.textStyles.h4.copyWith(
+              fontWeight: FontWeight.w600,
+              fontSize: 12, // Ridotto il font per adattare il titolo
+            ),
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+      ],
     );
   }
 }
